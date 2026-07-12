a1c87e3492d43d9af42da4d1370e660530505aba
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "static_web_app_function_app_registrations_id" {
+  description = "Map of id values across all static_web_app_function_app_registrations, keyed the same as var.static_web_app_function_app_registrations"
+  value       = { for k, v in azurerm_static_web_app_function_app_registration.static_web_app_function_app_registrations : k => v.id }
+}
 output "static_web_app_function_app_registrations_function_app_id" {
   description = "Map of function_app_id values across all static_web_app_function_app_registrations, keyed the same as var.static_web_app_function_app_registrations"
   value       = { for k, v in azurerm_static_web_app_function_app_registration.static_web_app_function_app_registrations : k => v.function_app_id }
